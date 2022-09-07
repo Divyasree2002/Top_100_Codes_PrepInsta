c695b6cadf39fc1e7971007001caf8d94b6d09a9
V.Divya Sree <76731589+Divyasree2002@users.noreply.github.com>
2022-09-07 19:28:00 +0530

    Add files via upload

---
+++ 06_Greatest_2_numbers.py
@@ -0,0 +1,9 @@
+m = int(input("Enter 1st number : "))
+n = int(input("Enter 2nd number : "))
+
+if m > n :
+  print(str(m), "is the greatest.")
+else :
+  print(str(n), "is the greatest.")
+
+#print(max(m,n))  -> direct function in python
